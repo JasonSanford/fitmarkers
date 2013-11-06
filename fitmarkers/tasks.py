99985754212f3c63262dcc5a2a8e3f55efdea35a
Jason Sanford <jason.sanford@mapmyfitness.com>
2013-11-06 00:07:06 -0700

    MapMyFitness API working. Succesfully getting workouts and routes via OAuth1

--- fitmarkers/tasks.py
+++ fitmarkers/tasks.py
@@ -13,7 +13,8 @@ from markers.models import Marker, WorkoutMarker
 from remote import Providers
 from remote.oauth.runkeeper import RunKeeperAPI
 from remote.oauth.mapmyfitness import MapMyFitnessAPI
-from remote.runkeeper.utils import path_to_geojson, date_string_to_datetime
+from remote.runkeeper import utils as rk_utils
+from remote.mapmyfitness import utils as mmf_utils
 from utils import get_last_monday
 
 
@@ -63,30 +64,26 @@ def get_new_runkeeper_workouts(social_auth_users, since_date):
                 pass
             if raw_workout['has_path']:
                 raw_workout_full = rk_api.get(raw_workout['uri']).json()
-                workouts_to_create[workout_id] = raw_workout_full
+                workouts_to_create[workout_id] = {
+                    'user': sau.user,
+                    'provider': Providers.RUNKEEPER,
+                    'provider_id': workout_id,
+                    'start_datetime': rk_utils.date_string_to_datetime(raw_workout['start_time']),
+                    'duration': int(raw_workout['duration']),
+                    'geom': GEOSGeometry(json.dumps(rk_utils.path_to_geojson(raw_workout_full['path']))),
+                }
             else:
                 no_path_workout_count += 1
         logger.info('Creating {0} workouts for {1}.'.format(len(workouts_to_create), sau.user))
         logger.info('Ignoring {0} workouts for {1} because they already exist.'.format(existing_workout_count, sau.user))
         logger.info('Ignoring {0} workouts for {1} because there is no path.'.format(no_path_workout_count, sau.user))
-        #create_workouts.apply_async(raw_workouts)
         create_workouts(sau, workouts_to_create, Providers.RUNKEEPER)
 
 
 @task(name='create_workouts')
 def create_workouts(social_auth_user, raw_workouts, provider):
     for provider_id, raw_workout in raw_workouts.iteritems():
-        path_geojson = path_to_geojson(raw_workout['path'])
-        path_linestring = GEOSGeometry(json.dumps(path_geojson))
-        workout_kwargs = {
-            'user': social_auth_user.user,
-            'provider': provider,
-            'provider_id': provider_id,
-            'start_datetime': date_string_to_datetime(raw_workout['start_time']),
-            'duration': int(raw_workout['duration']),
-            'geom': path_linestring,
-        }
-        workout = Workout(**workout_kwargs)
+        workout = Workout(**raw_workout)
         workout.save()
         check_workout_for_markers.apply_async((workout,))
         logger.info('Created {0} for {1}'.format(workout, social_auth_user.user))
@@ -101,38 +98,39 @@ def get_new_mmf_workouts(social_auth_users, since_date):
             'created_after': '{0}-{1}-{2}T00:00:00+00:00'.format(since_date.year, since_date.month, since_date.day)
         }
         encoded_params = urllib.urlencode(url_params)
-        activities_response = mmf_api.get('/v7.0/workout/?{0}'.format(encoded_params))
-        logger.info('activities_response: %s' % activities_response.json())
-        #raw_workouts = activities_response.json()['items']
-        #logger.info('raw_workouts: %s' % raw_workouts)
-        """
+        workouts_response = mmf_api.get('/v7.0/workout/?{0}'.format(encoded_params)).json()
+        raw_workouts = workouts_response['_embedded']['workouts']
         existing_workout_count = 0
-        no_path_workout_count = 0
+        no_time_series_workout_count = 0
         workouts_to_create = {}
         for raw_workout in raw_workouts:
-            uri_parts = raw_workout['uri'].split('fitnessActivities/')
-            workout_id = int(uri_parts[1])
+            workout_id = raw_workout['_links']['self'][0]['id']
             #
             # We could use get_or_create here, but there's no sense going through
             # process of parsing the workout geometry if we don't have to.
             #
             try:
-                existing_workout = Workout.objects.get(provider_id=workout_id, provider=Providers.RUNKEEPER)
+                existing_workout = Workout.objects.get(provider_id=workout_id, provider=Providers.MAPMYFITNESS)
                 existing_workout_count += 1
                 continue
             except Workout.DoesNotExist:
                 pass
-            if raw_workout['has_path']:
-                raw_workout_full = rk_api.get(raw_workout['uri']).json()
-                workouts_to_create[workout_id] = raw_workout_full
+            if raw_workout['has_time_series']:
+                workout_route = mmf_api.get('{0}?field_set=detailed'.format(raw_workout['_links']['route'][0]['href'])).json()
+                workouts_to_create[workout_id] = {
+                    'user': sau.user,
+                    'provider': Providers.MAPMYFITNESS,
+                    'provider_id': workout_id,
+                    'start_datetime': mmf_utils.date_string_to_datetime(raw_workout['start_datetime']),
+                    'duration': int(raw_workout['aggregates']['active_time_total']),
+                    'geom': GEOSGeometry(json.dumps(mmf_utils.points_to_geojson(workout_route['points']))),
+                }
             else:
-                no_path_workout_count += 1
+                no_time_series_workout_count += 1
         logger.info('Creating {0} workouts for {1}.'.format(len(workouts_to_create), sau.user))
         logger.info('Ignoring {0} workouts for {1} because they already exist.'.format(existing_workout_count, sau.user))
-        logger.info('Ignoring {0} workouts for {1} because there is no path.'.format(no_path_workout_count, sau.user))
-        #create_workouts.apply_async(raw_workouts)
+        logger.info('Ignoring {0} workouts for {1} because there is no time series.'.format(no_time_series_workout_count, sau.user))
         create_workouts(sau, workouts_to_create, Providers.RUNKEEPER)
-        """
 
 
 @task(name='check_workout_for_markers')
