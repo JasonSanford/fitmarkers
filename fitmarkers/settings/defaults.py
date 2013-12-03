bb0b90f5a74395116e8fb60de907b316cb25b766
Jason Sanford <jasonsanford@gmail.com>
2013-12-02 21:57:47 -0700

    Adding middleware to settings

--- fitmarkers/settings/defaults.py
+++ fitmarkers/settings/defaults.py
@@ -95,6 +95,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'fitmarkers.middleware.ImpersonateMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
