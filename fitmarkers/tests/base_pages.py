3564aa1e583fede6455da61507ac4cd7753f1651
Jason Sanford <jasonsanford@gmail.com>
2013-12-07 23:16:38 -0700

    Use redis during tests

--- fitmarkers/tests/base_pages.py
+++ fitmarkers/tests/base_pages.py
@@ -11,17 +11,14 @@ from fitmarkers.user.views import dashboard
 class BasePagesTest(TestCase):
     def setUp(self):
         # TODO: need to start redis to hit a lot of pages
-        """
-        subprocess.Popen('redis-server /path/to/redis-test.config, shell=True,
-                         stdin=subprocess.PIPE,
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE,
-                         cwd=cwd)
-        """
+        self.redis_process = subprocess.Popen('redis-server redis-test.conf', shell=True)
         self.client = Client()
         self.factory = RequestFactory()
         self.jason = UserFactory(id=1, first_name='Jason', last_name='Sanford', is_superuser=True)
         self.ellen = UserFactory(id=2, first_name='Ellen', last_name='Sanford')
+
+    def tearDown(self):
+        self.redis_process.kill()
 
     def test_home(self):
         resp = self.client.get(reverse('home'))
@@ -33,12 +30,3 @@ class BasePagesTest(TestCase):
         response = home(request)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['Location'], reverse('user_dashboard'))
-
-    """
-    def test_impersonation_for_superuser_works(self):
-        request = self.factory.get('{0}?__impersonate='.format(reverse('user_dashboard'), self.ellen.pk))
-        request.user = self.jason
-        response = dashboard(request)
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue('Ellen' in response.content)
-    """
--- fitmarkers/user/tests/factories.py
+++ fitmarkers/user/tests/factories.py
@@ -8,3 +8,4 @@ lasts = ('Sanford', 'Crafton', 'Edwin', 'Zubich',)
 class UserFactory(factory.Factory):
     FACTORY_FOR = User
 
+    username = factory.Sequence(lambda n: 'user{0}'.format(n))
